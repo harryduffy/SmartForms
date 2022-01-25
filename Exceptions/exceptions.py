9060187364a57f6d20115b1efb0f21d322f07d84
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-01-25 20:49:00 +1100

    Add files via upload

---
+++ Exceptions/exceptions.py
@@ -0,0 +1,9 @@
+class CSRFTokenMissingException(Exception):
+    
+    def __init__(self, message):
+        super().__init__(message)
+
+class ContentNull(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
