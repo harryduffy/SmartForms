21d00e9a82ca78207d3a01a9559f4d42852b7c1d
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-01-31 21:36:11 +1100

    merge

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('web_error_templates/404.html')
+
+@app.errorhandler(403)
+def resource_forbidden(e):
+    return render_template('web_error_templates/403.html')
